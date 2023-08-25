1569d10032558432d9eb1c87fd455af955250575
yogiflogic <yogiflogic792gmail.com>
2023-08-25 18:25:28 +0700

    update service-mesh.sh

--- service-mesh/service-mesh.sh
+++ service-mesh/service-mesh.sh
@@ -1,4 +1,16 @@
+#add resource memory RAM minikube
+minikube config set memory 4000
+
 #Instalasi Istio :
+
+curl -L https://istio.io/downloadIstio | sh -
+cd istio-1.17.1
+export PATH=$PWD/bin:$PATH
+or
+nano .bashrc
+export PATH="$PATH:/home/xxxx/istio-1.18.2/bin"
+
+#menginstal Istio mengunakan profile bertipe demo
 istioctl install --set profile=demo -y
 
 #Deploy Aplikasi Bookinfo :
@@ -18,6 +30,9 @@ kubectl apply -f bookinfo-gateway.yaml
 #memastikan tak ada issue pada konfigurasi Istio
 istioctl analyze
 
+#Minikube tunnel yang akan mengirim traffic ke Istio Ingress Gateway
+#Proses ini akan memberikan kita sebuah external load balancer (EXTERNAL-IP)
+#untuk Kubernetes service bernama istio-ingressgateway di dalam namespace istio-system.
 minikube tunnel
 
 #simpan nilai-nilai yang diperlukan ke dalam environment variable di terminal window sebelumnya
@@ -30,7 +45,7 @@ minikube tunnel
 echo "$INGRESS_HOST" "$INGRESS_PORT" "$SECURE_INGRESS_PORT"
 127.0.0.1 80 443
 
-#menentukan nilai untuk environment variable bernama GATEWAY_URLdari nilai IP address dan port Istio Ingress Gateway sebelumnya.
+#menentukan nilai untuk environment variable bernama GATEWAY_URL dari nilai IP address dan port Istio Ingress Gateway sebelumnya.
 export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
 
 #Cek apakah nilai IP address dan port untuk Istio Ingress Gateway sudah benar tersimpan pada GATEWAY_URL
@@ -54,3 +69,15 @@ kubectl rollout status deployment/kiali -n istio-system
 #Agar mempermudah proses visualisasi trace data, ubah Traffic metrics per refresh menjadi Last 1h dan Refresh interval menjadi Every 10s
 istioctl dashboard kiali
 http://localhost:20001/kiali
+
+
+#untuk melihat trace data, kita harus mengirim sejumlah request ke aplikasi
+#Dengan default sampling rate sebesar 1%, kita harus mengirim setidaknya 100 request sebelum trace pertama dapat terlihat
+#untuk mengirim 100 request ke aplikasi (tepatnya ke productpageservice)
+#coba hentikan dulu Kiali dashboard di terminal (dengan kombinasi CTRL+C), kemudian jalankan perintah berikut
+for i in $(seq 1 100); do curl -s -o /dev/null "http://$GATEWAY_URL/productpage"; done
+
+#jalankan kembali Kiali dashboard
+istioctl dashboard kiali
+
+#Menerapkan Request Routing :
